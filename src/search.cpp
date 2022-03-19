169bf29707011478a36ec5826026003b86fb2293
KJE-98 <>
2022-03-18 22:36:45 -0400

    increase LMR reduction when a good enough line has been found

--- src/search.cpp
+++ src/search.cpp
@@ -1170,8 +1170,8 @@ moves_loop: // When in check, search starts here
           if (PvNode && !ss->inCheck && abs(ss->staticEval - bestValue) > 250)
               r--;
 
-          if ( bestValue - rootEval > 240 && (ss->ply)%2 == 0 )
-              r += 2 + (bestValue - rootEval > 450);
+          if ( bestValue - rootEval > 250 && (ss->ply)%2 == 0 )
+              r += 2 + (bestValue - rootEval > 500);
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
