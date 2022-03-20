6870fab5eee6b9ef8e925b983c5f154724bbe5a0
KJE-98 <>
2022-03-20 19:00:29 -0400

    cap the max reduction in lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,7 @@ moves_loop: // When in check, search starts here
                        : cutNode && moveCount <= 8 ? 1
                        :                             0;
 
-          r = std::min(r,9);
+          r = std::min(r,10);
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
