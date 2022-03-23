06590cb05a7bc3f8b13f109f22ff752504eefd2e
KJE-98 <>
2022-03-23 12:42:53 -0400

    increase lmr when good move is found

--- src/search.cpp
+++ src/search.cpp
@@ -1175,7 +1175,7 @@ moves_loop: // When in check, search starts here
           if (   rootEvalDiff > 240
               && bestValue >= alpha
               && ss->ply % 2 == 0
-              && ( depth == 3 || depth == 4 ) ) {
+              && depth < 5 ) {
                 r += 1;
               }
 
