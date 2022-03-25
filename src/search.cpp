0d1d129da8ca36f7e3929fe5e3e0f1475436d6cd
KJE-98 <>
2022-03-25 13:10:05 -0400

    squeeze r as r gets higher

--- src/search.cpp
+++ src/search.cpp
@@ -1180,6 +1180,8 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
+          r = r - ( r > 7 ) - ( r > 10 ) - ( r > 13);
+
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
