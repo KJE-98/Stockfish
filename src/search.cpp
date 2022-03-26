00bf3c41e57fe7dff9ab38f12763c87a89d5db37
KJE-98 <>
2022-03-25 20:22:43 -0400

    take 4 - squeeze r as r gets higher

--- src/search.cpp
+++ src/search.cpp
@@ -1182,7 +1182,7 @@ moves_loop: // When in check, search starts here
           r -= ss->statScore / 15914;
 
           if ( depth < 20 )
-              r = r - ( r > 8 ) - ( r > 12 ) - ( r > 15) - ( r > 18);
+              r = r - ( r > 9 ) - ( r > 12 ) - ( r > 15);
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
