f697a38e33f9eaaf5a9fafe422359d2fa3dc3692
KJE-98 <>
2022-03-27 12:31:42 -0400

    extend nodes in a critical range- take 5

--- src/search.cpp
+++ src/search.cpp
@@ -1112,8 +1112,10 @@ moves_loop: // When in check, search starts here
               extension = 1;
 
           if (   depth == 1
+                 && abs(alpha) < 350
+                 && abs(beta) > 200
                  && abs(ss->staticEval) > 250
-                 && abs(ss->staticEval) < 350 )
+                 && abs(ss->staticEval) < 300 )
               extension += 1;
       }
 
