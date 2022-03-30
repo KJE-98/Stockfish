644af00d255c381fc720fccfcc9ade8848461d5a
KJE-98 <>
2022-03-30 04:22:24 -0400

    Incorporate static eval improvment into statscore reduction - take 2

--- src/search.cpp
+++ src/search.cpp
@@ -1185,7 +1185,7 @@ moves_loop: // When in check, search starts here
           }
           rootEvalDiff = std::clamp(rootEvalDiff, -400, 400);
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / ( 15914 - 15 * rootEvalDiff );
+          r -= ss->statScore / ( 15914 - 20 * rootEvalDiff );
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
