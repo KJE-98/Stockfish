d0f8f22c0a1ac0f0e3a8b68c3aa2836ba9700298
KJE-98 <>
2022-03-31 02:35:32 -0400

    weighted average top three best values - take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1383,8 +1383,8 @@ moves_loop: // When in check, search starts here
 
     topThree[1] = std::max(topThree[1], topThree[0]-400);
     topThree[2] = std::max(topThree[2], topThree[0]-400);
-    if (moveCount > 3){
-      return Value(topThree[0]*8/10 + topThree[0]*1/10 + topThree[0]*1/10);
+    if (moveCount > 2 && depth > 12){
+      return Value(topThree[0]*9/10 + topThree[0]*1/20 + topThree[0]*1/20);
     }
     return bestValue;
   }
