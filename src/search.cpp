bae3a673409a57446806e795161a93197dfbb593
KJE-98 <>
2022-04-05 21:49:20 -0400

    use confidence in time man (fixed) - take 6

--- src/search.cpp
+++ src/search.cpp
@@ -1182,6 +1182,9 @@ moves_loop: // When in check, search starts here
           if (PvNode && !ss->inCheck && abs(ss->staticEval - bestValue) > 250)
               r--;
 
+          if (*certainty > 1)
+              r += 2;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
