82cbf1366e6315956bf17f9db673ea0e73d1a1f2
KJE-98 <>
2022-04-06 23:42:37 -0400

    use confidence in time man, zero margin - take 3

--- src/search.cpp
+++ src/search.cpp
@@ -471,7 +471,7 @@ void Thread::search() {
                                               * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);
-          double certaintyFactor = std::clamp((7.0 - certainty)/7.0, 0.6, 1.0);
+          double certaintyFactor = std::clamp((8.0 - certainty)/8.0, 0.6, 1.0);
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition * certaintyFactor;
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
           // yielding correct scores and sufficiently fast moves.
