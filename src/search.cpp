6473afbba30191135cdbef22a66e14ac422058d9
KJE-98 <>
2022-04-09 01:32:04 -0400

    Use certainty in time man - no searh - take 7

--- src/search.cpp
+++ src/search.cpp
@@ -38,11 +38,11 @@
 namespace Stockfish {
 
 namespace Search {
-  int certaintyDenom = 12;
-  int certaintyMin = 5;
-  int certaintyDepthMultiplier = 2;
-  int certaintyDepthMin = 10;
-  int certaintyAlpha = 300;
+  int certaintyDenom = 10;
+  int certaintyMin = 7;
+  int certaintyDepthMultiplier = 3;
+  int certaintyDepthMin = 8;
+  int certaintyAlpha = 250;
 
   LimitsType Limits;
 }
@@ -478,7 +478,6 @@ void Thread::search() {
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);
           double certaintyFactor = std::clamp((1.0 * certaintyDenom - certainty)/certaintyDenom, 1.0 * certaintyMin / 10, 1.0);
-
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition * certaintyFactor;
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
           // yielding correct scores and sufficiently fast moves.
