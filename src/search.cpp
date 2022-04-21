7bdec1ee8f525be3bc349f6333bc9e56e7c14503
KJE-98 <>
2022-04-21 14:00:45 -0400

    In non-PV, late in search, reduce more based on movenumber and depth

--- src/search.cpp
+++ src/search.cpp
@@ -1178,8 +1178,8 @@ moves_loop: // When in check, search starts here
               r -= 15 / ( 3 + depth );
 
           // Increase reduction if NonPV and moveNumber and ply are both high.
-          if (!PvNode && moveCount > 3 && ss->ply > 13)
-              r += 3;
+          if (!PvNode && moveCount > 3 && ss->ply > 15)
+              r += depth / 4;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
