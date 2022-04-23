8d477a757adc0e9faa9e4c4753bb1f6c655d34ab
KJE-98 <>
2022-04-22 22:55:54 -0400

    replace nonPV with Tri-search nodes - take 2

--- src/search.cpp
+++ src/search.cpp
@@ -851,7 +851,7 @@ namespace {
     // Step 10. ProbCut (~4 Elo)
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   !PvNode && !triNode
+    if (   !PvNode
         &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
