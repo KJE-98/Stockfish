0e7d4c9d5fdb228422bdeec6987111c53ba85cc2
KJE-98 <>
2022-04-24 14:48:33 -0400

    reduction of depth if delta is small

--- src/search.cpp
+++ src/search.cpp
@@ -993,10 +993,13 @@ moves_loop: // When in check, search starts here
       movedPiece = pos.moved_piece(move);
       givesCheck = pos.gives_check(move);
 
+      Value delta = beta - alpha;
+
+
       // Calculate new depth for this move
       newDepth = depth - 1;
-
-      Value delta = beta - alpha;
+      if (bestMove && PvNode && depth > 2)
+          newDepth -= (delta < 30) + (delta < 15);
 
       // Step 14. Pruning at shallow depth (~98 Elo). Depth conditions are important for mate finding.
       if (  !rootNode
