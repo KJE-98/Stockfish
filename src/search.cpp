651980f049c9e06ec71bcd54dae1d70783e718d3
KJE-98 <>
2022-05-02 03:51:50 -0400

    extend killer + pbMoves bench: 7665297

--- src/search.cpp
+++ src/search.cpp
@@ -1138,6 +1138,11 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5491)
               extension = 1;
+
+          else if (    depth > 9
+                    && move == ss->killers[0]
+                    && pbMove)
+              extension = 1;
       }
 
       // Add extension to new depth
