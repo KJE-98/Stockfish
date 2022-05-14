66b73b2c5bb12f46bebba9241067c352ab382733
KJE-98 <>
2022-05-14 00:03:02 -0400

    do probcut at some pvnodes bench: 6788455

--- src/search.cpp
+++ src/search.cpp
@@ -850,7 +850,7 @@ namespace {
     // Step 10. ProbCut (~4 Elo)
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   !PvNode
+    if (   (!PvNode || (depth < 10 && ss->staticEval > probCutBeta && (ss-2)->staticEval > probCutBeta))
         &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
