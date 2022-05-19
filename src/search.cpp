07cc99f2f6230c247196d834bb29010d23a41b21
KJE-98 <>
2022-05-18 23:09:48 -0400

    final stuff

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (thisThread->rootDepth > 7)
+          if (thisThread->rootDepth > 10)
               r -= similarity / 30;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
