0cdc38215517958f5d4b45ad4bd32a50761b083a
KJE-98 <>
2022-07-02 19:52:48 -0400

    try lmr adjustment in trisearch bench: 5162394

--- src/search.cpp
+++ src/search.cpp
@@ -1183,6 +1183,9 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
+          if (triSearch)
+              r -= 1;
+
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
           // beyond the first move depth. This may lead to hidden double extensions.
