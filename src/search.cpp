958bd7a3185a5e415cf2bd4c66a46f7c74c90a09
KJE-98 <>
2022-07-03 09:05:11 -0400

    triSearch every other rootDepth, with ttfix and lmr adjustment bench: 4445374

--- src/search.cpp
+++ src/search.cpp
@@ -1183,10 +1183,8 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
-          if (triSearch)
-              r -= 2;
-          else if (PvNode)
-              r++;
+          if (PvNode)
+              r += triSearch ? -1 : 1;
 
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
