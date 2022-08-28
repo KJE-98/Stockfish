61291a4d3fd41a41913375e79d5f4a2b60a278cc
KJE-98 <>
2022-08-28 17:46:23 -0400

    use total movecount of previously searched moves to adjust reductions take 7 bench: 5200177

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (bestOffset < -15)
+          if (bestOffset < -8)
             r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
@@ -1275,7 +1275,7 @@ moves_loop: // When in check, search starts here
             bestOffset = (ss+1)->offset;
 
       if ( !bestMove )
-            ss->offset = std::min(3, moveCount) - bestOffset;
+            ss->offset = std::min(5, moveCount) - bestOffset * 9 / 10;
 
       if (value > bestValue)
       {
@@ -1285,7 +1285,7 @@ moves_loop: // When in check, search starts here
           {
               bestMove = move;
 
-              ss->offset = std::min(3, moveCount) - (ss+1)->offset;
+              ss->offset = std::min(5, moveCount) - (ss+1)->offset * 9 / 10;
 
               if (PvNode && !rootNode) // Update pv even in fail-high case
                   update_pv(ss->pv, move, (ss+1)->pv);
