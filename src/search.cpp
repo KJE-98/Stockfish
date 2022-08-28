b4f1bf58a5c7cc668bf9f0a9c802b6d94f59c01c
KJE-98 <>
2022-08-28 02:36:45 -0400

    use total movecount of previously searched moves to adjust reductions bench: 5160942

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (bestOffset < -80)
+          if (bestOffset < -110)
               r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
