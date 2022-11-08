f52663a816c50a313169c4ddf734ad080f9cf28e
kje-98 <eghbalniakian@gmail.com>
2022-11-07 21:23:18 -0600

    more reduction when we have a failed ttMove in a pvNode bench: 3865669

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           // Decrease reduction for PvNodes based on depth
-          if (PvNode)
+          if (PvNode && !bestMove)
               r -= 1 + 11 / (3 + depth);
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
