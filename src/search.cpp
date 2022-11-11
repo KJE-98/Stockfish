5c93d6022803b4f8af62095e6367a2299bc4d45a
kje-98 <eghbalniakian@gmail.com>
2022-11-10 20:35:12 -0600

    emphasize from-to in movepicker if complexity is low bench:4638125

--- src/search.cpp
+++ src/search.cpp
@@ -749,9 +749,6 @@ namespace {
             tte->save(posKey, VALUE_NONE, ss->ttPv, BOUND_NONE, DEPTH_NONE, MOVE_NONE, eval);
     }
 
-    if (complexity < 0)
-        sync_cout << complexity << sync_endl;
-
     thisThread->complexityAverage.update(complexity);
 
     // Use static evaluation difference to improve quiet move ordering (~3 Elo)
