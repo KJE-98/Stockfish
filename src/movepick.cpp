2ec8959aa1f31465a9835fea781bac3b932bb527
kje-98 <eghbalniakian@gmail.com>
2022-11-10 19:19:55 -0600

    emphasize from-to in povepicker when complexity is low bench: 4421786

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -126,12 +126,11 @@ void MovePicker::score() {
   for (auto& m : *this)
       if constexpr (Type == CAPTURES) {
           m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
-                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
-                   +     3 * complexity;
+                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
       }
 
       else if constexpr (Type == QUIETS)
-          m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
+          m.value = (2 + 3 * ( complexity < 20)) * (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
