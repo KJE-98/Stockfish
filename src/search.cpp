7e68770adc6edce9f798c5e8747d6d43b9de29d2
kje-98 <eghbalniakian@gmail.com>
2022-11-12 17:57:19 -0600

    complexity based margin idea bench: 4539057

--- src/search.cpp
+++ src/search.cpp
@@ -1273,14 +1273,7 @@ moves_loop: // When in check, search starts here
 
               if (PvNode && value < beta) // Update alpha! Always alpha < beta
               {
-                  alpha = value;
-
-                  // Reduce other moves if we have found at least one score improvement
-                  if (   depth > 1
-                      && depth < 6
-                      && beta  <  VALUE_KNOWN_WIN
-                      && alpha > -VALUE_KNOWN_WIN)
-                     depth -= 1;
+                  alpha = std::min( value + 5 * ( complexity > 100 ), VALUE_KNOWN_WIN );
 
                   assert(depth > 0);
               }
