998210cfaf2c41e6b7c463b03d67d7dc7e1b07a8
kian eghbalnia <eghbalniakian@gmail.com>
2023-08-04 15:26:42 -0500

    make negative use of stat bonus a little smaller

--- src/search.cpp
+++ src/search.cpp
@@ -1212,7 +1212,7 @@ moves_loop: // When in check, search starts here
               if (newDepth > d)
                   value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
 
-              int bonus = value <= alpha ? -stat_bonus(newDepth)
+              int bonus = value <= alpha ? -std::min(300 * d - 400, 1000)
                         : value >= beta  ?  stat_bonus(newDepth)
                                          :  0;
 
