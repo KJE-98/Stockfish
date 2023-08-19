9130cd37818c4fb6492a38b3dd2c0a6f0cb2cb7f
kian eghbalnia <eghbalniakian@gmail.com>
2023-08-18 23:36:50 -0500

    more agressive fmc at cutnodes bench:1493904

--- src/search.cpp
+++ src/search.cpp
@@ -975,7 +975,7 @@ moves_loop: // When in check, search starts here
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
           int fmc = futility_move_count(improving, depth);
           if (cutNode)
-              fmc = fmc * 2 / 3;
+              fmc = fmc * 1 / (2 + depth > 4);
           moveCountPruning = moveCount >= fmc;
 
           // Reduced depth of the next LMR search
