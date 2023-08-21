e758382e270d44b5cd3f9127d72cb5ae16bdf73f
kian eghbalnia <eghbalniakian@gmail.com>
2023-08-21 02:07:48 -0500

    Don't propogate cutnodes bench:1849861

--- src/search.cpp
+++ src/search.cpp
@@ -1191,7 +1191,7 @@ moves_loop: // When in check, search starts here
               newDepth += doDeeperSearch - doShallowerSearch + doEvenDeeperSearch;
 
               if (newDepth > d)
-                  value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
+                  value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, PvNode);
 
               int bonus = value <= alpha ? -stat_bonus(newDepth)
                         : value >= beta  ?  stat_bonus(newDepth)
@@ -1208,7 +1208,7 @@ moves_loop: // When in check, search starts here
           if (!ttMove && cutNode)
               r += 2;
 
-          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3), !cutNode);
+          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3), PvNode);
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail
