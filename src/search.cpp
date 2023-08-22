b7fe123b2b14bb9c613700ccf460e47b98554931
kian eghbalnia <eghbalniakian@gmail.com>
2023-08-22 18:08:16 -0500

    new histories heuristic bench:1498497

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
           if ((ss-1)->responses[4] != SQ_NONE)
               movePairReduction += thisThread->movepairHistory[(ss-1)->responses[4]][(ss-1)->responses[5]][to_sq((ss-1)->currentMove)][to_sq(move)];
 
-          movePairReduction = std::clamp(movePairReduction / 50, -2, 2);
+          movePairReduction = std::clamp(movePairReduction / 50, -2, 3);
 
           r -= movePairReduction;
       }
@@ -1347,9 +1347,9 @@ moves_loop: // When in check, search starts here
 
           if( is_ok((ss-1)->currentMove) && !rootNode)
           {
-            thisThread->movepairHistory[(ss-1)->responses[0]][(ss-1)->responses[1]][to_sq((ss-1)->currentMove)][to_sq(move)] << -4;
-            thisThread->movepairHistory[(ss-1)->responses[2]][(ss-1)->responses[3]][to_sq((ss-1)->currentMove)][to_sq(move)] << -4;
-            thisThread->movepairHistory[(ss-1)->responses[4]][(ss-1)->responses[5]][to_sq((ss-1)->currentMove)][to_sq(move)] << -4;
+            thisThread->movepairHistory[(ss-1)->responses[0]][(ss-1)->responses[1]][to_sq((ss-1)->currentMove)][to_sq(move)] << -1;
+            thisThread->movepairHistory[(ss-1)->responses[2]][(ss-1)->responses[3]][to_sq((ss-1)->currentMove)][to_sq(move)] << -1;
+            thisThread->movepairHistory[(ss-1)->responses[4]][(ss-1)->responses[5]][to_sq((ss-1)->currentMove)][to_sq(move)] << -1;
           }
               
       }
